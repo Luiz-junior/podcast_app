fd24da864f7a2c556de426945f6c5ed7cea76c76
luiz-junior <junior.rodrigues0222@gmail.com>
2019-09-04 19:04:43 -0300

    updated version

--- android/app/src/main/java/com/podcast/MainApplication.java
+++ android/app/src/main/java/com/podcast/MainApplication.java
@@ -1,4 +1,4 @@
-package com.podcast_app;
+package com.podcast;
 
 import android.app.Application;
 import android.util.Log;
@@ -7,8 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
